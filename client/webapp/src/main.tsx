10e5bb78024b642c1e4bf1e2c8cfdb5a8421e93d
Philippe Shampanier <pshampanier@gmail.com>
2023-12-04 09:57:53 -0800

    core refactoring

--- client/webapp/src/main.tsx
+++ client/webapp/src/main.tsx
@@ -1,7 +1,6 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import App from "../../common/src/App";
+import "../../common/src/index.css";
 import "@/utils/monaco-workers";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
